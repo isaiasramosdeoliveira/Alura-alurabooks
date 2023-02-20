a3ee4e7b62977ad875f48126c2a6c554710b5d99
Isaías ramos <isaiasramosdeoliverira2003@gmail.com>
2023-02-20 12:15:31 -0300

    Acréscimo dos valores totais

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 const list = document.querySelector(".content-books")
 const btns = document.querySelector(".content-btns")
 const button = btns.querySelectorAll(".btns-button")
+const totalPayable = document.querySelector(".menu_total")
 
 let dice = []
 let filterDice = []
-
+let total = 0
+let status = ''
 async function loadBooks() {
     const res = await fetch("https://guilhermeonrails.github.io/casadocodigo/livros.json")
     dice = await res.json()
     displayBooks(dice);
 }
 loadBooks()
-
-function discount(value) {
-    const dice = value - (value * 0.3)
-    return dice
-}
 
 for (let i = 0; i < 3; i++) {
     button[i].addEventListener("click", (e) => {
@@ -26,14 +23,20 @@ for (let i = 0; i < 3; i++) {
         })
         console.log(filterDice);
         displayBooks(filterDice);
+        total = filterDice.reduce((total, value) => {
+            return total + value.preco;
+        }, 0)
+        totalPayable.innerHTML = `R$ ${total.toFixed(2)}`
     })
 }
 
 function displayBooks(dice) {
+    dice = dice.sort((a, b) => a.preco - b.preco)
     dice.forEach(value => {
-        const newValue = discount(value.preco)
+        value.quantidade === 0 ? status = 'disabled'
+        : status = ''
         list.innerHTML += `
-        <div class = "book"> 
+        <div class = "book ${status}"> 
             <img src = "${value.imagem}"
         alt = "${value.alt}" > 
             <div class = "describe"> 
